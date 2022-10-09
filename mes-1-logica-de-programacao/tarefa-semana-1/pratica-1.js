86e7d79dc76b8bad02e6aa758a8a2c392ec6d26a
Erick marques <erick.marquesrodrigues@hotmail.com>
2022-10-09 16:52:07 -0300

    exibindo o valor das variáveis para o usuário

--- mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
@@ -1,6 +1,11 @@
 const nome = "Erick marques Rodrigues"
 const email ="erick.marquesrodrigues7@hotmail.com"
-const profissão ="Vendedor/ varejo"
+const profissao ="Vendedor/ varejo"
 const idade = 30
 const lugar ="tenho a vontade de conhecer a Alemanha"
 
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(lugar)
