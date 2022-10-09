096ca48ee59bb8a2c269f30af4912d9b6026e993
Erick marques <erick.marquesrodrigues@hotmail.com>
2022-10-09 16:38:20 -0300

    Declarando variáveis

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
@@ -0,0 +1,6 @@
+const nome = "Erick marques Rodrigues"
+const email ="erick.marquesrodrigues7@hotmail.com"
+const profissão ="Vendedor/ varejo"
+const idade = 30
+const lugar ="tenho a vontade de conhecer a Alemanha"
+
