ff759b8a7639f799911ca71fbbecfa792a12f0ab
Erick marques <erick.marquesrodrigues@hotmail.com>
2022-10-09 17:47:41 -0300

    operadores de comparação

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-3.js
@@ -0,0 +1,11 @@
+const nome = "Erick marques Rodrigues" == "Erick marques Rodrigues"
+const email ="erick.marquesrodrigues7@hotmail.com" =="erick.marquesrodrigues7@hotmail.com"
+const profissao ="Vendedor/ varejo" == "Vendedor/ varejo"
+const idade = 30 ==""
+const lugar ="tenho a vontade de conhecer a Alemanha" == "tenho a vontade de conhecer a Alemanha"
+
+console.log (nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(lugar)
