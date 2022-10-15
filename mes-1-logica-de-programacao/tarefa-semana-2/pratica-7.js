ff269c68c6bee62aa4bdfb4b3a91b0bfc060db2d
Erick marques <erick.marquesrodrigues@hotmail.com>
2022-10-15 16:20:52 -0300

    if else

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-7.js
@@ -0,0 +1,13 @@
+let saudacao = function(nome){
+    if(nome == "en" ){
+        console.log("Hellow world!");
+
+    }else if(nome == "es"){
+        console.log("Hola mundo");
+
+    }else if(nome == "pt"){
+        console.log("Olá mundo!");
+    }else
+       console.log("Olá mundo!")
+}     
+saudacao("pt");
