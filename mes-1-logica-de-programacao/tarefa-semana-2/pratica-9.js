8587cf6bbefe109a49f71a1f2933a37f2eb22d9b
Erick marques <erick.marquesrodrigues@hotmail.com>
2022-10-16 17:03:19 -0300

    função analise de número maior

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-9.js
@@ -0,0 +1,8 @@
+function AnaliseMaiorNumero(numero1 , numero2){
+    if(numero1 >= numero2){
+    console.log(`Número: ${numero1} é maior que número${numero2}`);
+    }else{
+    console.log(`Número: ${numero2} é maior que o número:${numero1}`);
+}
+}
+AnaliseMaiorNumero(30 , 35);
