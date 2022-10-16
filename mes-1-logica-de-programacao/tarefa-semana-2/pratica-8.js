98a9c922d4bc60ed785cde94188462e370fbdf1e
Erick marques <erick.marquesrodrigues@hotmail.com>
2022-10-16 17:02:18 -0300

    operação lógica par, impar

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-8.js
@@ -0,0 +1,8 @@
+function analiseNumero(numero){
+    if(numero % 2 == 0){
+    console.log(`O número ${numero} é par.`);
+    }else{
+    console.log(`O número ${numero} é impar`);
+    }
+}
+analiseNumero(29);
