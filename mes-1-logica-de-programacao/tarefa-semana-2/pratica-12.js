f5fcd72623dbac59dafeb9060be351b8bbf9ba98
Erick Marques R <erick.marquesrodrigues7@hotmail.com>
2022-10-17 23:19:56 -0300

    função recebe um numero e retorna o dia da semana, comando switch utilizado

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-12.js
@@ -0,0 +1,35 @@
+let diaDaSemana = function(numero){
+    switch (numero){
+        case 1:
+        console.log("Segunda");
+        break;
+    
+        case 2:
+        console.log("Terça");
+        break;
+    
+        case 3:
+        console.log("Quarta");
+        break;
+    
+        case 4:
+        console.log("Quinta");
+        break;
+    
+        case 5:
+        console.log("Sexta");
+        break;
+    
+        case 6:
+        console.log("Sábado");
+        break;
+    
+        case 7:
+        console.log("Domingo");
+        break;
+    
+        default:
+        console.log(`número ${numero} não é válido para dia de semana`);
+    }
+    }   
+    diaDaSemana(4);
