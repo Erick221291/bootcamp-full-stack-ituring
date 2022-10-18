ac07718ea223569b8ad28b2824d4c5be17af35f8
Erick Marques R <erick.marquesrodrigues7@hotmail.com>
2022-10-17 23:30:58 -0300

    função compara 2 númesros e retorna o menor,utilizado operador ternário

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-13.js
@@ -0,0 +1,5 @@
+let comparandoNumerosEretornandoOmenor = function(a , b){
+    let menor = a < b ? a : b;
+    console.log(`O número: ${menor} é menor!`)
+}
+comparandoNumerosEretornandoOmenor(90,10);
