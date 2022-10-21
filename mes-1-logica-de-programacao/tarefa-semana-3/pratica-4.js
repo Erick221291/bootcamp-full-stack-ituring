410e167676550a5e7ea0c8ddad6131990af7eb53
Erick Marques R <erick.marquesrodrigues7@hotmail.com>
2022-10-20 21:48:46 -0300

    teste

---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-4.js
@@ -0,0 +1,10 @@
+function exibirlista(lista){
+    for(let i =0; i<lista.length; i++){
+        console.log(`[${i}] ${lista}`)
+    }
+}
+
+
+
+let lista =["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"]
+exibirlista(lista);
