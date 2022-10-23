aafe39915d5a3147503572bd92da8587b20f5cf4
Erick Marques R <erick.marquesrodrigues7@hotmail.com>
2022-10-23 20:01:09 -0300

    exibição de lista melhorada

--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-4.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-4.js
@@ -1,10 +1,8 @@
 function exibirlista(lista){
     for(let i =0; i<lista.length; i++){
-        console.log(`[${i}]`+ lista[i])
+        console.log(`[${i}] `+ lista[i])
     }
 }
-
-
 
 let lista =["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"]
 exibirlista(lista);
