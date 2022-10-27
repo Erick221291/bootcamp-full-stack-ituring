ee405d95f24246a91720d7bcb43a9fc0526056c8
Erick Marques R <erick.marquesrodrigues7@hotmail.com>
2022-10-27 18:56:52 -0300

    usando o metodo forEach

---
+++ mes-1-logica-de-programacao/tarefa-semana-4/prtatica-1.js
@@ -0,0 +1,11 @@
+function exibirlista(lista){
+    lista.forEach(function(lista,i) {
+        
+        console.log(`[${i}] ${lista}`) });
+        
+    }
+
+
+let lista =["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"]
+exibirlista(lista);
+
